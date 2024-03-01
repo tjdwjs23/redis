7a33e3f5ea98e4df43664bdd22db5e172ef75a05
SeonghunChoi <77057201+tjdwjs23@users.noreply.github.com>
2024-03-01 21:28:02 +0900

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-BUILD_JAR=$(ls /home/ec2-user/projectName/build/libs/*.jar)
+BUILD_JAR=$(ls /home/ec2-user/template/build/libs/*.jar)
 JAR_NAME=$(basename $BUILD_JAR)
-echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/projectName/deploy.log
+echo "> build 파일명: $JAR_NAME" >> /home/ec2-user/template/deploy.log
 
-echo "> build 파일 복사" >> /home/ec2-user/projectName/deploy.log
+echo "> build 파일 복사" >> /home/ec2-user/template/deploy.log
 DEPLOY_PATH=/home/ec2-user/sictak/
 cp $BUILD_JAR $DEPLOY_PATH
 
-echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/projectName/deploy.log
+echo "> 현재 실행중인 애플리케이션 pid 확인" >> /home/ec2-user/template/deploy.log
 CURRENT_PID=$(pgrep -f $JAR_NAME)
 
 if [ -z $CURRENT_PID ]
 then
-  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/projectName/deploy.log
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다." >> /home/ec2-user/template/deploy.log
 else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
@@ -20,6 +20,6 @@ else
 fi
 
 DEPLOY_JAR=$DEPLOY_PATH$JAR_NAME
-echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/projectName/deploy.log
+echo "> DEPLOY_JAR 배포"    >> /home/ec2-user/template/deploy.log
 chmod +x $DEPLOY_JAR
-nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/projectName/deploy_err.log &
+nohup java -jar $DEPLOY_JAR >> /home/ec2-user/deploy.log 2>/home/ec2-user/template/deploy_err.log &

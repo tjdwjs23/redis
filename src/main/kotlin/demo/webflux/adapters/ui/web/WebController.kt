0c1b3d3a60a96cb9deef0d08e6ba5262e5100066
Seonghun <tjdwjs23@sugarbricks.ne.kr>
2024-03-03 10:13:36 +0900

    refactor(swagger) : default path

---
+++ src/main/kotlin/demo/webflux/adapters/ui/web/WebController.kt
@@ -0,0 +1,13 @@
+package demo.webflux.adapters.ui.web
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+
+//@RestController
+//internal class SwaggerRedirector {
+//    @GetMapping
+//    fun api(): String {
+//        return "redirect:http://localhost:8080/webjars/swagger-ui/index.html"
+//    }
+//}

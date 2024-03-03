86b82d8cb384a0eaee608b6ff2bb951b8dc9de12
Seonghun <tjdwjs23@sugarbricks.ne.kr>
2024-03-03 20:26:35 +0900

    refactor(swagger) : Tag Settings

--- src/main/kotlin/demo/webflux/adapters/ui/board/BoardController.kt
+++ src/main/kotlin/demo/webflux/adapters/ui/board/BoardController.kt
@@ -3,31 +3,35 @@ package demo.webflux.adapters.ui.board
 import demo.webflux.application.usecases.board.service.BoardService
 import demo.webflux.ports.input.BoardRequest
 import demo.webflux.ports.output.BoardResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/board")
+@RequestMapping("/v1/board")
+@Tag(name = "BoardController", description = "게시판 관련 API")
 class BoardController(private val boardService: BoardService) {
     /**
      * 게시글 저장
-     * @param board 게시글
-     * @return 저장된 게시글
+     * @param boardRequest 게시글
+     * @return Mono<BoardResponse> 저장된 게시글
      */
     @PostMapping
+    @Operation(summary = "게시글 저장", description = "새 게시글을 저장합니다.")
     fun post(
-        @RequestBody boardRequest: BoardRequest,
+            @RequestBody boardRequest: BoardRequest,
     ): Mono<BoardResponse> {
         return boardService.save(boardRequest)
     }
 
     /**
      * 모든 게시글 조회
-     * @return 게시글 목록
-     * @throws RuntimeException 게시글 조회에 실패했을 때
+     * @return Flux<BoardResponse> 게시글 목록
      */
     @GetMapping
+    @Operation(summary = "모든 게시글 조회", description = "모든 게시글을 조회합니다.")
     fun getAll(): Flux<BoardResponse> {
         return boardService.getAll()
     }
@@ -35,12 +39,12 @@ class BoardController(private val boardService: BoardService) {
     /**
      * 특정 게시글 조회
      * @param id 게시글의 고유 식별자
-     * @return 게시글
-     * @throws RuntimeException 게시글 조회에 실패했을 때
+     * @return Mono<BoardResponse> 게시글
      */
     @GetMapping("/{id}")
+    @Operation(summary = "특정 게시글 조회", description = "특정 게시글을 조회합니다.")
     fun getById(
-        @PathVariable id: Long,
+            @PathVariable id: Long,
     ):  Mono<BoardResponse> {
         return boardService.getById(id)
     }
@@ -48,11 +52,12 @@ class BoardController(private val boardService: BoardService) {
     /**
      * 게시글 삭제
      * @param id 게시글의 고유 식별자
-     * @throws RuntimeException 게시글 삭제에 실패했을 때
+     * @return Mono<Boolean> 게시글 삭제 성공 여부
      */
     @DeleteMapping("/{id}")
+    @Operation(summary = "게시글 삭제", description = "특정 게시글을 삭제합니다.")
     fun deleteById(
-        @PathVariable id: Long,
+            @PathVariable id: Long,
     ): Mono<Boolean> {
         return boardService.deleteById(id)
     }
@@ -60,14 +65,14 @@ class BoardController(private val boardService: BoardService) {
     /**
      * 게시글 수정
      * @param id 게시글의 고유 식별자
-     * @param updatedBoard 수정된 게시글
-     * @return 수정된 게시글
-     * @throws RuntimeException 게시글 수정에 실패했을 때
+     * @param boardRequest 수정된 게시글
+     * @return Mono<BoardResponse> 수정된 게시글
      */
     @PutMapping("/{id}")
+    @Operation(summary = "게시글 수정", description = "특정 게시글을 수정합니다.")
     fun updateById(
-        @PathVariable id: Long,
-        @RequestBody boardRequest: BoardRequest,
+            @PathVariable id: Long,
+            @RequestBody boardRequest: BoardRequest,
     ): Mono<BoardResponse> {
         return boardService.updateById(id, boardRequest)
     }
--- src/main/kotlin/demo/webflux/adapters/ui/user/UserController.kt
+++ src/main/kotlin/demo/webflux/adapters/ui/user/UserController.kt
@@ -3,6 +3,8 @@ package demo.webflux.adapters.ui.user
 import demo.webflux.application.usecases.user.service.UserService
 import demo.webflux.ports.input.UserRequest
 import demo.webflux.ports.output.UserResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -11,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/v1/user")
+@Tag(name = "UserController", description = "사용자 관련 API")
 class UserController(private val userService: UserService) {
 
     /**
@@ -20,6 +23,7 @@ class UserController(private val userService: UserService) {
      * @return 등록된 사용자
      */
     @PostMapping("/signup")
+    @Operation(summary = "사용자 등록", description = "새로운 사용자를 등록합니다.")
     fun signup(
             @RequestBody userRequest: UserRequest,
     ): Mono<UserResponse> {
@@ -32,6 +36,7 @@ class UserController(private val userService: UserService) {
      * @return 토큰
      */
     @PostMapping("/login")
+    @Operation(summary = "로그인", description = "사용자 로그인을 수행하고 토큰을 반환합니다.")
     fun login(
         @RequestBody userRequest: UserRequest
     ): Mono<String> {

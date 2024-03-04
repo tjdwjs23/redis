83b1a37adee19aaf58414b3482e1e0a9d05e5f52
tjdwjs23 <tjdwjs23>
2024-03-04 09:19:41 +0900

    fix(docker) : docker-compose.yml update (maria-db ADD)

--- src/main/kotlin/demo/webflux/application/repositories/user/UserRepository.kt
+++ src/main/kotlin/demo/webflux/application/repositories/user/UserRepository.kt
@@ -2,8 +2,10 @@ package demo.webflux.application.repositories.user
 
 import demo.webflux.domain.user.UserEntity
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import org.springframework.stereotype.Repository
 import reactor.core.publisher.Mono
 
+@Repository
 interface UserRepository: ReactiveCrudRepository<UserEntity, String> {
     fun findByUsername(username: String): Mono<UserEntity>
 }

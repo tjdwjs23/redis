032cbfdcca38880d5a2c4783a99926c0869ce3d2
Seonghun <tjdwjs23@sugarbricks.ne.kr>
2024-03-09 21:44:26 +0900

    refactor(security) : spring security settings

--- sql/board.sql
+++ sql/board.sql
@@ -2,6 +2,7 @@ CREATE TABLE `BOARD` (
      `ID` bigint(20) NOT NULL AUTO_INCREMENT,
      `TITLE` varchar(200) DEFAULT NULL,
      `CONTENT` text DEFAULT NULL,
+     `WRITE_ID` varchar(100) DEFAULT NULL,
      `CREATED_DATE` datetime NOT NULL DEFAULT current_timestamp() COMMENT '생성일',
      `UPDATED_DATE` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp() COMMENT '수정일',
      PRIMARY KEY (`ID`)
